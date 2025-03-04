68fde20e538c4d05e48a63598ba43925b51120f7
Amanda Lima <51892828+AmandaLimasiva@users.noreply.github.com>
2025-03-03 23:22:25 -0300

    adicionando função para alterar informações - patch

--- livros.js
+++ livros.js
@@ -1,9 +1,11 @@
-const express = require("express") //Iniciando o Express
-const router = express.Router() //Configurando a primeira parte da rota
+//const express = require("express"); //Iniciando o Express
+import express from "express";
+const router = express.Router(); //Configurando a primeira parte da rota
 import { v4 as uuidv4 } from 'uuid'; //Biblioteca para criar id
 
 
 const app = express() //Iniciando o app
+app.use(express.json())
 const porta = 3333  //Criando a porta
 
 //Criando array inicial de livros
@@ -28,6 +30,13 @@ const livros = [
         quantPag: 552,
         imagem: 'https://www.imdb.com/pt/title/tt1014759/',
         resenha: 'Uma menina, um coelho e uma história capazes de fazer qualquer um de nós voltar a sonhar. Alice é despertada de um leve sono ao pé de uma árvore por um coelho peculiar. '
+    },
+    {
+        id:'4',
+        nome: 'Astronomia',
+        quantPag: 552,
+        imagem: 'https://www.imdb.com/pt/title/tt1014759/',
+        resenha: 'Amo Astronomia'
     }
 ]
 
@@ -56,9 +65,39 @@ function mostraPorta (){
     response.json(livros)
  }
 
+ //PATCH
+ function alteraLivro(request, response){
+    function localizaLivro(livro){ //encontra os livros pelo Id
+        if(livro.id === request.params.id){
+            return livro
+        }
+    }
+
+    const livroLocalizado = livros.find(localizaLivro)
+
+    if(request.body.nome){
+        livroLocalizado.nome = request.body.nome
+    }
+
+    if(request.body.quantPag){
+        livroLocalizado.quantPag = request.body.quantPag
+    }
+
+    if(request.body.imagem){
+        livroLocalizado.imagem = request.body.imagem
+    }
+
+    if(request.body.resenha){
+        livroLocalizado.resenha = request.body.resenha
+    }
+
+    response.json(livros) //Lista é enviada atualizada
+ }
+ 
 
 app.use(router.get('/livros', mostraLivros)) // Configuração da segunda parte da rota //GET
 app.use(router.post('/livros',criaLivro)) //Insere um novo livro via post //POST
+app.use(router.patch('/livros/:id',alteraLivro)) //Altera o livro //PATCH
 app.listen(porta, mostraPorta) // Servidor ouvindo a porta 3333
 //mostraPorta()
 
